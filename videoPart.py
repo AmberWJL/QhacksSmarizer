ca031b2a0cdc7ec09acb685d3d8f2115cb36f578
Wanqing Li <71136622+eus-lwq@users.noreply.github.com>
2022-01-29 14:47:42 -0500

    Update videoPart.py

--- videoPart.py
+++ videoPart.py
@@ -5,83 +5,105 @@ import pprint
 from configure import auth_key
 from time import sleep
 
+"""This code is from Assembly AI tutorial: https://www.youtube.com/watch?v=CrLmgrGiVVY&t=214s"""
+
 if 'status' not in st.session_state:
     st.session_state['status'] = 'submitted'
-    st.session_state['save_location'] = '' 
+
 ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192', 
-    }],
-    'ffmpeg-location': './',
-    'outtmpl': "./%(id)s.%(ext)s",
+   'format': 'bestaudio/best',
+   'postprocessors': [{
+       'key': 'FFmpegExtractAudio',
+       'preferredcodec': 'mp3',
+       'preferredquality': '192',
+   }],
+   'ffmpeg-location': './',
+   'outtmpl': "./%(id)s.%(ext)s",
+}
+
+transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
+upload_endpoint = 'https://api.assemblyai.com/v2/upload'
+
+headers_auth_only = {'authorization': auth_key}
+headers = {
+   "authorization": auth_key,
+   "content-type": "application/json"
 }
 CHUNK_SIZE = 5242880
-
-upload_endpoint = "https://api.assemblyai.com/v2/upload"
-transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
-
-headers = {
-    "authorization": auth_key,
-    "content-type":"application/json"
-}
-
-@st.cache
-def download_audio(link):
-    _id = link.strip()
-    def get_vid(_id):
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            return ydl.extract_info(_id)
-        #download audio part of video to local
-    meta = get_vid(_id)
-    st.session_state['save_location'] = meta['id']+".mp3"
-    print('saved video(mp3) to', st.session_state['save_location'])
-        
-def read_file(filename):
-    with open(filename,'rb') as _file:
-        while True:
-            data = _file.read(CHUNK_SIZE)
-            if not data:
-                break
-            yield data
  
-#access file and upload
-def start_transcription():
-    upload_response = requests.post(
-        upload_endpoint,
-        headers=headers, data=read_file(st.session_state['save location'])           
-    )
+@st.cache
+def transcribe_from_link(link, categories: bool):
+	_id = link.strip()
 
-    audio_url = upload_response.json()['upload_url']
-    print("Uploaded to", audio_url)
-    
-    transcript_request = {
-        'audio_url': audio_url,
-        'lab_categories': 'False',
-    }
-    #send request
-    transcript_response = request.post(transcript_endpoint, json=transcript_request, headers=headers)
-    
-    transcript_id = transcript_response.json()['id']
-    polling_endpoint = transcript_endpoint + "/" + transcript_id
-    return polling_endpoint
-            
-st.title("Easily transcribe Youtube videos")
-#this is a random video length 23s 480p
-link = st.text_input("Enter your youtube video link", "https://www.youtube.com/watch?v=WX7-3E6cirQ")
+	def get_vid(_id):
+		with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+			return ydl.extract_info(_id)
 
-download_audio(link)
+	# download the audio of the YouTube video locally
+	meta = get_vid(_id)
+	save_location = meta['id'] + ".mp3"
 
-#start
-polling_endpoint = start_transcription()
-#update the session state
+	print('Saved mp3 to', save_location)
 
-while st.session_state['status']!='completed':
-    polling_response = requests.get(polling_endpoint, headers=headers)
-    st.session_state['status'] = polling_response.json()['status']
-    print(polling_response.json()['status'])
-    transcription = polling_response.json()['text']
 
-st.markdown(transcription)
+	def read_file(filename):
+		with open(filename, 'rb') as _file:
+			while True:
+				data = _file.read(CHUNK_SIZE)
+				if not data:
+					break
+				yield data
+
+
+	# upload audio file to AssemblyAI
+	upload_response = requests.post(
+		upload_endpoint,
+		headers=headers_auth_only, data=read_file(save_location)
+	)
+
+	audio_url = upload_response.json()['upload_url']
+	print('Uploaded to', audio_url)
+
+	# start the transcription of the audio file
+	transcript_request = {
+		'audio_url': audio_url,
+		'iab_categories': 'True' if categories else 'False',
+	}
+
+	transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
+
+	# this is the id of the file that is being transcribed in the AssemblyAI servers
+	# we will use this id to access the completed transcription
+	transcript_id = transcript_response.json()['id']
+	polling_endpoint = transcript_endpoint + "/" + transcript_id
+
+	print("Transcribing at", polling_endpoint)
+
+	return polling_endpoint
+
+
+def get_status(polling_endpoint):
+	polling_response = requests.get(polling_endpoint, headers=headers)
+	st.session_state['status'] = polling_response.json()['status']
+
+def refresh_state():
+	st.session_state['status'] = 'submitted'
+
+
+st.title('Easily transcribe YouTube videos')
+
+link = st.text_input('Enter your YouTube video link', 'https://youtu.be/dccdadl90vs', on_change=refresh_state)
+st.video(link)
+
+st.text("The transcription is " + st.session_state['status'])
+
+polling_endpoint = transcribe_from_link(link, False)
+
+st.button('check_status', on_click=get_status, args=(polling_endpoint,))
+
+transcript=''
+if st.session_state['status']=='completed':
+	polling_response = requests.get(polling_endpoint, headers=headers)
+	transcript = polling_response.json()['text']
+
+st.markdown(transcript)
